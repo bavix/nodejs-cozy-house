c7747a4093eea40547f55a1735eb607d8cf09271
Babichev Maxim <babichev@mail.tvil.ru>
2018-09-14 16:16:01 +0300

    remove old code

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 var express = require('express');
-var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var sassMiddleware = require('node-sass-middleware');
 
 var routes = require('./src/routes/index');
 
