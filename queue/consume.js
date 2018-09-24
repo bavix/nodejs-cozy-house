51e1b26b9ac41f83f581bf3c28bcc4f7e2d16e29
Babichev Maxim <babichev@mail.tvil.ru>
2018-09-24 18:53:57 +0300

    update consume

--- queue/consume.js
+++ queue/consume.js
@@ -1,3 +1,31 @@
+const os = require('os');
 const amqp = require('amqplib');
+const term = require('./signals/term');
+const hup = require('./signals/hup');
+const queue = amqp.connect(process.env.QUEUE_URL);
 
-module.exports = amqp.connect(process.env.QUEUE_URL);
+queue.then((conn) => {
+
+    const terminate = term(conn);
+    const hangUp = hup(conn);
+
+    // signals
+    process.once('SIGTERM', terminate);
+    process.once('SIGINT', terminate);
+    process.once('SIGHUP', hangUp);
+
+    return conn.createChannel().then(function (ch) {
+
+        var queue = ch.assertQueue(process.env.QUEUE_NAME, {durable: true});
+
+        queue = queue.then(function () {
+            ch.prefetch(1);
+        });
+
+        // todo
+
+    }).catch(terminate);
+
+});
+
+module.exports = queue;
