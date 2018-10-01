ecfc2a98266a6141bd47ca3769b634a75c85c482
Babichev Maxim <babichev@mail.tvil.ru>
2018-10-01 13:41:20 +0300

    remove console.log

--- store/eventStore.js
+++ store/eventStore.js
@@ -37,7 +37,6 @@ class EventStore extends Store {
             'INSERT INTO ' + schema + '.events (' + keys.join(', ') + ')',
             {format: 'JSONEachRow'},
             (error) => {
-                console.log(error);
                 if (error) {
                     const timestamp = +new Date();
                     const pid = process.pid;
