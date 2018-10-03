883e492c5249ef76d26b8a8bfe0a2288cdca410d
Babichev Maxim <babichev@mail.tvil.ru>
2018-10-03 12:02:16 +0300

    patch cli

--- cli.js
+++ cli.js
@@ -3,28 +3,34 @@
 // load env
 require('dotenv').load();
 
-const db = require('./lib/db');
+const knex = require('./lib/db');
 const uuid = require('uuid/v4');
 const yargs = require('yargs');
-
-function result(res) {
-    console.info(res);
-    process.exit(0);
-}
 
 yargs.command('$0 <name>', 'Utility to add target (get API key)', (yargs) => {
     yargs.positional('name', {
         describe: 'The name of the target',
         type: 'string'
     })
-}, ({ name }) => {
-    db.one('SELECT name, token FROM targets WHERE name=$1', name)
-        .then(result)
-        .catch((error) => {
-            db.one('INSERT INTO targets (name, token) VALUES($1, $2) RETURNING name, token', [name, uuid()])
-                .then(result)
-                .catch(console.error);
-        });
+}, async ({ name }) => {
+    await knex('targets')
+        .select('name', 'token')
+        .where({name})
+        .first()
+        .then(async (row) => {
+            if (row) {
+                console.log(row);
+            } else {
+                await knex('targets')
+                    .insert({name, token: uuid()})
+                    .returning(['name', 'token'])
+                    .then(([row]) => console.log(row))
+                    .catch(console.error);
+            }
+        })
+        .catch(console.error);
+
+    process.exit(0);
 });
 
 yargs.version('version', '1.0.0');
