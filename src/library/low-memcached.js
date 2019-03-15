8cda2e762bc85aacdb96e017a120c2bbe8692489
Babichev Maxim <babichev@mail.tvil.ru>
2019-03-15 18:35:07 +0300

    ttl memcached

--- src/library/low-memcached.js
+++ src/library/low-memcached.js
@@ -2,7 +2,12 @@ import { promisify } from 'util'
 import Memcached from 'memcached'
 import env from './env'
 
-const mc = new Memcached(['127.0.0.1:11211'])
+const mc = new Memcached(['127.0.0.1:11211'], {
+  timeout: 50,
+  retries: 0,
+  failures: 0,
+  retry: 0
+})
 
 const memcachedGet = promisify(mc.get).bind(mc)
 const memcachedSet = promisify(mc.set).bind(mc)
