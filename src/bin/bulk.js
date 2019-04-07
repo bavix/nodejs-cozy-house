7b5ee4d3196daa5a1dbde8cf89acdddabbc3a210
Babichev Maxim <info@babichev.net>
2019-04-07 23:13:29 +0300

    add empty files

--- src/bin/bulk.js
+++ src/bin/bulk.js
@@ -1,13 +1,16 @@
 #!/usr/bin/env node
+import sigterm from '../queue/signals/sigterm'
+import sigint from '../queue/signals/sigint'
+import sighup from '../queue/signals/sighup'
 import bagger from '../library/bagger'
 import { env } from '../library/env'
 import amqp from '../queue/consume'
 
 let store = []
 
-// process.once('SIGTERM', _sigterm(conn));
-// process.once('SIGINT', _sigint(conn));
-// process.once('SIGHUP', _sighup(conn));
+process.once('SIGTERM', sigterm(store))
+process.once('SIGINT', sigint(store))
+process.once('SIGHUP', sighup(store))
 
 amqp(env.QUEUE_HANDLER, function(queue, message) {
   if (message.properties.type !== undefined) {
